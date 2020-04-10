767ddb342abfb501a84f8bcb9209531d40758b2d
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2020-04-10 03:27:17 -0500

    Project scaffold?

---
+++ src/GradleBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GradleBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
